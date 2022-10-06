a45dde1277cf20fa2c0c113fc12e52de6707ca18
Jamal .S <me@shrjamal.com>
2022-10-06 13:26:23 +0100

    libft atoit test

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,13 +6,15 @@
 /*   By: jasahrao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 10:57:38 by jasahrao          #+#    #+#             */
-/*   Updated: 2022/10/06 11:01:10 by jasahrao         ###   ########.fr       */
+/*   Updated: 2022/10/06 13:16:18 by jasahrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 static int	ft_isspace(char c)
 {
-	if (c == ' ' || (c >= '\t' || c <= '\r'))
+	if (c == ' ' || (c >= '\t' && c <= '\r'))
 		return (1);
 	return (0);
 }
@@ -21,19 +23,37 @@ int	ft_atoi(const char *str)
 {
 	int	res;
 	int	sign;
+	int	i;
 
 	sign = 1;
 	res = 0;
-	while (ft_isspace(*str))
-		str++;
-	if (*str == '-')
-		sign = -1;
-	else if (*str == '+')
-		str++;
-	while (*str >= '0' && *str <= '9')
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-')
 	{
-		res = (res * 10) + (*str - '0');
-		str++;
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (res * 10) + (str[i] - '0');
+		i++;
 	}
 	return (res * sign);
 }
+
+// int	main(void)
+// {
+// 	printf("$%d$\n", ft_atoi("-1325632167"));
+// 	printf("$%d$\n", ft_atoi("1325632167"));
+// 	printf("$%d$\n", ft_atoi("-2147483648"));
+// 	printf("$%d$\n", ft_atoi("\t-1234ab567"));
+// 	printf("$%d$\n", ft_atoi("\n+1234ab567"));
+// 	printf("$%d$\n", ft_atoi("2147483647"));
+// 	printf("$%d$\n", ft_atoi("0123a456789"));
+// 	printf("$%d$\n", ft_atoi("a123"));
+// 	printf("$%d$\n", ft_atoi(""));
+// }
