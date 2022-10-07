a4c4924d3afe64bc54714928a5fc1e5bcbff6dc9
Jamal .S <me@shrjamal.com>
2022-10-07 15:20:10 +0100

    ft_strlcat

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jasahrao <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/07 15:14:50 by jasahrao          #+#    #+#             */
+/*   Updated: 2022/10/07 15:19:51 by jasahrao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dstsize == 0 || dstsize <= dst_len)
+		return src_len + dstsize;
+	i = 0;
+	while (src[i] &&  i < dst_len + dstsize - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return src_len + dstsize;
+}
