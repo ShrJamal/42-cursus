160899eac2d6c6849103a4c9a4fdfa55f0ee65f3
Jamal .S <me@shrjamal.com>
2022-10-08 19:18:18 +0100

    libft ft_strjoin

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jasahrao <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/08 19:11:55 by jasahrao          #+#    #+#             */
+/*   Updated: 2022/10/08 19:18:08 by jasahrao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*res;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2)
+		return (0);
+	res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		res[i++] = s2[j++];
+	res[i] = '\0';
+	return (res);
+}
+
+// int	main(void)
+// {
+// 	printf("%s\n", ft_strjoin("Hello ", "World!"));
+// 	printf("%s\n", ft_strjoin("A", "B"));
+// 	printf("%s\n", ft_strjoin("", "B"));
+// 	printf("%s\n", ft_strjoin("A", ""));
+// 	printf("%s\n", ft_strjoin("", ""));
+// }
