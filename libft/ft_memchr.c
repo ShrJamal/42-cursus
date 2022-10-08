235a3cd0f13f479f0e828d87956613753eafe023
ShrJamal <me@shrjamal.com>
2022-10-08 21:34:04 +0100

    add libft.h

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jasahrao <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 14:56:41 by jasahrao          #+#    #+#             */
-/*   Updated: 2022/10/08 20:17:43 by jasahrao         ###   ########.fr       */
+/*   Updated: 2022/10/08 21:32:14 by jasahrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	return (0);
 }
 
-int main(){
-	printf("%s\n", (unsigned char*) ft_memchr("Hello", 'e', 5));
-	printf("%s\n", (unsigned char*) ft_memchr("Hello", 'l', 5));
-	printf("%s\n", (unsigned char*) ft_memchr("Hello", '\0', 5));
-}
+// int main(){
+// 	printf("%s\n", (unsigned char*) ft_memchr("Hello", 'e', 5));
+// 	printf("%s\n", (unsigned char*) ft_memchr("Hello", 'l', 5));
+// 	printf("%s\n", (unsigned char*) ft_memchr("Hello", '\0', 5));
+// }
