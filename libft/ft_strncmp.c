e594267153f995a03e2ce7408b484473ff4a024d
Jamal .S <me@shrjamal.com>
2022-10-11 13:06:26 +0100

    fix libft strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 16:14:08 by jasahrao          #+#    #+#             */
-/*   Updated: 2022/10/11 10:57:49 by jasahrao         ###   ########.fr       */
+/*   Updated: 2022/10/11 13:06:11 by jasahrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
 		i++;
 	}
 	return (0);
