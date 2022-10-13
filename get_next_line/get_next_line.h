9d3196ada57b2f81cc9136b4908c00ed4257e421
ShrJamal <me@shrjamal.com>
2022-10-13 11:39:50 +0100

    get_next_line init

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/13 11:17:12 by jasahrao          #+#    #+#             */
+/*   Updated: 2022/10/13 11:20:11 by jasahrao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
