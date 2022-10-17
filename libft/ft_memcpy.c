7f78f49e2c0657cfbd3c9bc4503a87c7313dc76d
Jamal .S <me@shrjamal.com>
2022-10-17 09:31:14 +0100

    [libft] remove restrict keyword

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 08:03:24 by jasahrao          #+#    #+#             */
-/*   Updated: 2022/10/11 10:52:45 by jasahrao         ###   ########.fr       */
+/*   Updated: 2022/10/17 09:31:02 by jasahrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void	*ft_memcpy(void * dst, const void * src, size_t n)
 {
 	size_t	i;
 
