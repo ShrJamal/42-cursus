57e211899d345fb33b15b028fce05803cce3b3f3
Jamal .S <me@shrjamal.com>
2022-10-23 09:04:37 +0100

    fix ft_memcpy segfault

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,18 +6,20 @@
 /*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 08:03:24 by jasahrao          #+#    #+#             */
-/*   Updated: 2022/10/17 09:31:02 by jasahrao         ###   ########.fr       */
+/*   Updated: 2022/10/23 08:58:20 by jasahrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void * dst, const void * src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
 	if (!dst && !src)
 		return (NULL);
+	if (src == dst)
+		return (dst);
 	i = 0;
 	while (i < n)
 	{
