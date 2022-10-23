299e58dadb7533c029ab8a68374eb00ce90e2bab
Jamal .S <me@shrjamal.com>
2022-10-23 09:49:34 +0100

    fix memmove seg fault

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 08:58:22 by jasahrao          #+#    #+#             */
-/*   Updated: 2022/10/11 10:52:45 by jasahrao         ###   ########.fr       */
+/*   Updated: 2022/10/23 09:15:09 by jasahrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		return (NULL);
 	s = (unsigned char *)src;
 	d = (unsigned char *)dst;
-	if (d < s)
+	if (d <= s)
 		ft_memcpy(dst, src, len);
 	else
 		while (len--)
