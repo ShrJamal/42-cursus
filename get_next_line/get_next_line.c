473c6f480c0bfb03e964e2b15d80c17097aa8826
ShrJamal <me@shrjamal.com>
2022-10-30 11:45:25 +0100

    [get_next_line] implement logic

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -5,12 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/13 11:17:41 by jasahrao          #+#    #+#             */
-/*   Updated: 2022/10/13 11:35:30 by jasahrao         ###   ########.fr       */
+/*   Created: 2022/10/30 10:50:22 by jasahrao          #+#    #+#             */
+/*   Updated: 2022/10/30 11:43:56 by jasahrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char	*get_next_line(int fd)
 {
-	return ("Hello World");
+	static char	*tmp;
+	char		*line;
+	char		*buff;
+	int			rd;
+	int			len;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	rd = 1;
+	while (rd != 0 && !ft_strchr(tmp, '\n'))
+	{
+		rd = read(fd, buff, BUFFER_SIZE);
+		if (rd == -1)
+			return (free(buff), NULL);
+		buff[rd] = '\0';
+		tmp = ft_strjoin(tmp, buff);
+	}
+	len = ft_strchr(tmp, '\n') - tmp + 1;
+	line = ft_substr(tmp, 0, len);
+	tmp = ft_substr(tmp, len, ft_strlen(tmp) - len + 1);
+	return (free(buff), line);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,17 +6,24 @@
 /*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 11:17:12 by jasahrao          #+#    #+#             */
-/*   Updated: 2022/10/13 11:20:11 by jasahrao         ###   ########.fr       */
+/*   Updated: 2022/10/30 11:38:33 by jasahrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4000
+# endif
 
 char	*get_next_line(int fd);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char *s, unsigned int start, size_t len);
 
 #endif
