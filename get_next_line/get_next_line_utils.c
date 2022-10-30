955914fb4b1017c6635d1bdff86e1f64db2af20a
ShrJamal <me@shrjamal.com>
2022-10-30 11:44:50 +0100

    [get_next_line] add strlen, strchr, strjoin, substr

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,92 @@
 /*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 11:17:36 by jasahrao          #+#    #+#             */
-/*   Updated: 2022/10/13 11:35:41 by jasahrao         ###   ########.fr       */
+/*   Updated: 2022/10/30 11:11:12 by jasahrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	if (!s)
+		return (NULL);
+	i = -1;
+	while (s[++i])
+		if (s[i] == c)
+			return ((char *)&s[i]);
+	if (c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		if (s1)
+			s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		str[i] = s1[i];
+	j = 0;
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	return (free(s1), str);
+}
+
+char	*ft_substr(char *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	slen;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (slen <= start)
+	{
+		sub = malloc(sizeof(char));
+		if (sub)
+			sub[0] = '\0';
+		return (sub);
+	}
+	else if (slen <= start + len)
+		slen -= start;
+	else
+		slen = len;
+	sub = malloc((slen + 1) * sizeof(char));
+	if (!sub)
+		return (NULL);
+	i = -1;
+	while (++i < len && s[start + i])
+		sub[i] = s[start + i];
+	sub[i] = '\0';
+	return (sub);
+}
