5b1d8313f39065612f6f896ef0e57b9194d1499a
Jamal .S <me@shrjamal.com>
2022-11-06 10:18:49 +0100

    [printf] cleanup puthex

--- printf/ft_puthex.c
+++ printf/ft_puthex.c
@@ -6,25 +6,18 @@
 /*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 12:22:59 by jasahrao          #+#    #+#             */
-/*   Updated: 2022/11/04 11:14:00 by jasahrao         ###   ########.fr       */
+/*   Updated: 2022/11/06 10:13:43 by jasahrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_printhex(unsigned int hex, char format)
+int	ft_puthex(unsigned int hex, char format)
 {
 	if (hex >= 16)
-		return (ft_printhex(hex / 16, format) + ft_printhex(hex % 16, format));
+		return (ft_puthex(hex / 16, format) + ft_puthex(hex % 16, format));
 	else if (format == 'X')
 		return (ft_putchar("0123456789ABCDEF"[hex]));
 	else
 		return (ft_putchar("0123456789abcdef"[hex]));
-}
-
-int	ft_puthex(unsigned int hex, char format)
-{
-	if (hex == 0)
-		return (write(1, "0", 1));
-	return (ft_printhex(hex, format));
 }
