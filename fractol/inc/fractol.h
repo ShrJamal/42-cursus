ea072e78811324c6281af03789fe4b99d7fc6342
othmane ait taleb <aittaleb108@gmail.com>
2023-01-23 09:37:09 +0100

    [fractol] add mlx library

--- fractol/inc/fractol.h
+++ fractol/inc/fractol.h
@@ -6,13 +6,14 @@
 /*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 08:53:47 by jasahrao          #+#    #+#             */
-/*   Updated: 2023/01/23 09:12:23 by jasahrao         ###   ########.fr       */
+/*   Updated: 2023/01/23 09:13:11 by jasahrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
+# include "../mlx/mlx.h"
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
--- fractol/src/main.c
+++ fractol/src/main.c
@@ -1,6 +1,11 @@
-#include "../inc/fractol.h"
+#include <mlx.h>
 
 int	main(void)
 {
-	printf("Hello");
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
