e3ee7755fdc58f046a33d4693b0638d50983201f
ShrJamal <me@shrjamal.com>
2023-01-23 09:53:14 +0100

    [fractor] make mlx

--- fractol/src/main.c
+++ fractol/src/main.c
@@ -3,9 +3,8 @@
 int	main(void)
 {
 	void	*mlx;
-	void	*mlx_win;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_new_window(mlx, 800, 600, "Fractol");
 	mlx_loop(mlx);
 }
