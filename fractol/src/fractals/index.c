87bc5ab8e73b54e5254795a81684cf9508b5492c
ShrJamal <me@shrjamal.com>
2023-03-04 17:30:06 +0100

    [fractol] add mandelbrot fractal

---
+++ fractol/src/fractals/index.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   index.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/03 22:14:45 by jasahrao          #+#    #+#             */
+/*   Updated: 2023/03/04 17:29:44 by jasahrao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../fractol.h"
+
+static void	ft_put_pixels(t_img *img, int x, int y, int iter)
+{
+	int	idx;
+
+	idx = (img->size_line * y) + x * 4;
+	img->addr[idx] = (iter * COLOR);
+	img->addr[idx + 1] = (iter * COLOR * 0.5);
+	img->addr[idx + 2] = (iter * COLOR * 14);
+	img->addr[idx + 3] = 0;
+}
+
+void	render_fractal(t_vars *fr)
+{
+	int x;
+	int y;
+	int iter;
+	t_cplx c;
+
+	x = 0;
+	while (x < WIN_SIZE)
+	{
+		c.re = fr->move.re + x * fr->scale;
+		y = 0;
+		while (y < WIN_SIZE)
+		{
+			c.im = fr->move.im - y * fr->scale;
+			if (fr->type == 1)
+				iter = mandelbrot(fr, c);
+			ft_put_pixels(&fr->mlx.img, x, y, iter);
+			y++;
+		}
+		x++;
+	}
+	mlx_put_image_to_window(fr->mlx.ptr, fr->mlx.win, fr->mlx.img.ptr, 0, 0);
+}
---
+++ fractol/src/fractals/mandelbrot.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mandelbrot.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/03 21:44:41 by jasahrao          #+#    #+#             */
+/*   Updated: 2023/03/04 11:52:21 by jasahrao         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../fractol.h"
+
+int	mandelbrot(t_vars *fr, t_cplx c)
+{
+	int			it;
+	t_cplx	z;
+
+	it = 0;
+	set_cplx(&z, 0, 0);
+	while (it <= fr->max_iter && sq_cplx(z) <= 4)
+	{
+		set_cplx(&z,
+					z.re * z.re - z.im * z.im + c.re,
+					2 * z.re * z.im + c.im);
+		it++;
+	}
+	return (it);
+}
