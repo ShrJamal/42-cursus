7eb6161c68b7657737d5be76e4f4cdf39454b5b2
ShrJamal <me@shrjamal.com>
2023-03-08 11:21:40 +0100

    [fractal] remove julia motion

--- fractol/src/hooks/mouse.c
+++ fractol/src/hooks/mouse.c
@@ -6,7 +6,7 @@
 /*   By: jasahrao <jasahrao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 21:26:04 by jasahrao          #+#    #+#             */
-/*   Updated: 2023/03/04 16:48:06 by jasahrao         ###   ########.fr       */
+/*   Updated: 2023/03/08 10:15:15 by jasahrao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,18 @@ static void	zoom(int btn, t_vars *fr, t_cplx p)
 	if (btn == MOUSE_UP)
 	{
 		zoom = 0.8;
-		fr->max_iter += 1;
+		fr->max_iter += 2;
 	}
 	else if (btn == MOUSE_DOWN)
 	{
 		zoom = 1.2;
-		fr->max_iter -= 1;
+		fr->max_iter -= 2;
 	}
 	else
 		return ;
 	fr->move.re = p.re + (fr->move.re - p.re) * zoom;
 	fr->move.im = p.im + (fr->move.im - p.im) * zoom;
 	fr->scale *= zoom;
-}
-
-static int	julia_motion(int x, int y, t_vars *fr)
-{
-	fr->c_julia.re = 4 * ((double)x / WIN_SIZE - 0.5);
-	fr->c_julia.im = 4 * ((double)(WIN_SIZE - y) / WIN_SIZE - 0.5);
-	return (0);
 }
 
 int	mouse_hook(int btn, int x, int y, t_vars *fr)
@@ -49,7 +42,7 @@ int	mouse_hook(int btn, int x, int y, t_vars *fr)
 	if (btn == MOUSE_UP || btn == MOUSE_DOWN)
 		zoom(btn, fr, p);
 	else if (btn == MOUSE_CLICK && fr->type == 2)
-		julia_motion(x, y, fr);
+		set_cplx(&fr->c_julia, p.re, p.im);
 	else
 		return (0);
 	render_fractal(fr);
